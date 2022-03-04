f7f128b56dd45f89f8993476b3e8528a93d727fd
abdelhamid <abdelhamidfg@gmail.com>
2022-03-04 16:56:33 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+## 3scale OPA Authorization Policy
+
+This Policy integrates APICast (3scale API Gateway) with  [Open Policy Agent](https://www.openpolicyagent.org/)  ,allows APICast to enforce authorization decisions (determine if access to an API should be granted or denied)  based on the defined policy in OPA.
+
+
+## OPA input structure
+ The policy sends the below json message structure in the body to the OPA API URL 
+  ```json          
+{
+   "input":{
+      "remote_addr":"10.129.0.1",
+      "method":"GET",
+      "request_id":"bfe7101ebf7fb94f72ccb65bb1769e19",
+      "path":"\/course",
+      "querystring":{
+         "foo":"bar"
+      },
+      "headers":{
+         "Authorization":"Token",
+         "Accept":"*\/*",
+         "User-Agent":"PostmanRuntime\/7.29.0"
+      }
+   }
+}
+```
+
+## How it works?
+![alt text](https://github.com/abdelhamidfg/keycloak-Authorization-policy/blob/master/opa-flow.jpg?raw=true)
+- The policy intercepts the client request sending http request to OPA URL as configured in the policy. 
+- The policy expects the policy evaluation result in the boolean formate  ,example allowed response {"result": true}
+- The pollicy will allow/deny the client request based on the reponse of OPA allow API .
+ 
+ ## Example of OPA policy 
+- The below example shows using of rego policy that allows GET method to the course resource with JWT username claim equals to alice.
+      ![alt text](https://github.com/abdelhamidfg/opa-Authorization-policy/blob/master/jwt-policy.jpg?raw=true)
+
+
---
+++ policies/opa/1.0.0/opa.lua
@@ -0,0 +1,69 @@
+local policy = require('apicast.policy')
+local _M = policy.new('opa', '1.0.0')
+local default_error_message = "Request blocked due to OPA Authorization policy"
+local new = _M.new
+
+function _M.new(config)
+    local self = new(config)
+    self.error_message = config.error_message or default_error_message
+    self.opa_url = config.opa_url
+    return self
+end
+
+local function isempty(s)
+    return s == nil or s == ''
+end
+
+local function deny_request(error_msg)
+    ngx.status = ngx.HTTP_FORBIDDEN
+    ngx.say(error_msg)
+    ngx.exit(ngx.status)
+end
+
+local function check_opa_authorization(opa_url, request_path)
+    local is_authorized = false
+    local ops = {}
+    local query = {}
+    local querystring = ngx.req.get_uri_args()
+    local headers = ngx.req.get_headers(0, true)
+    local cjson = require "cjson"
+    local json = cjson.encode({
+        input = {
+            method = ngx.req.get_method(),
+            path = request_path,
+            headers = headers or {},
+            querystring = querystring or {},
+            remote_addr = ngx.var.remote_addr,
+            request_id = ngx.var.request_id
+        }
+    })
+
+    ngx.log(ngx.DEBUG, "OPA input body in json=", json)
+    local httpc = require("resty.http").new()
+    local res, err = httpc:request_uri(opa_url, {
+        method = "POST",
+        body = json,
+        query = query,
+        headers = {}
+    })
+    if res and not isempty(res.body) then
+        ngx.log(ngx.DEBUG, "OPA API response body=", res.body)
+        local tab = cjson.decode(res.body)
+        if tab["result"] and tab["result"] == true then
+        --if string.find(res.body, "true") then
+            is_authorized = true
+        end
+    end
+    return is_authorized
+end
+
+function _M:access(context)
+    local is_opa_authorized = false
+    local request_path = context.original_request.path
+    is_opa_authorized = check_opa_authorization(self.opa_url, request_path)
+    if not is_opa_authorized then
+        return deny_request(self.error_message)
+    end
+end
+
+return _M
